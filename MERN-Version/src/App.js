94a6e3957615ed3fcfa8c21d15f6da9181c74e65
hncleary <clearyhunter@gmail.com>
2019-10-29 15:13:27 -0500

    Made Front Page Minimal

--- MERN-Version/src/App.js
+++ MERN-Version/src/App.js
@@ -6,36 +6,21 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import CreateTodo from "./components/create-todo.component";
 import EditTodo from "./components/edit-todo.component";
 import TodosList from "./components/todos-list.component";
+import { slide as Menu } from 'react-burger-menu';
 
-import logo from "./logo.svg";
+import logo from "./images/fmf-logo.png";
+import logoTitle from "./images/fmf-logo2.png"
 
 class App extends Component {
   render() {
     return (
-      <Router>
-        <div className="container">
-          <nav className="navbar navbar-expand-lg navbar-light bg-light">
-            <a className="navbar-brand" href="https://codingthesmartway.com" target="_blank">
-              <img src={logo} width="30" height="30" alt="CodingTheSmartWay.com" />
-            </a>
-            <Link to="/" className="navbar-brand">MERN-Stack Todo App</Link>
-            <div className="collpase navbar-collapse">
-              <ul className="navbar-nav mr-auto">
-                <li className="navbar-item">
-                  <Link to="/" className="nav-link">Todos</Link>
-                </li>
-                <li className="navbar-item">
-                  <Link to="/create" className="nav-link">Create Todo</Link>
-                </li>
-              </ul>
-            </div>
-          </nav>
-          <br/>
-          <Route path="/" exact component={TodosList} />
-          <Route path="/edit/:id" component={EditTodo} />
-          <Route path="/create" component={CreateTodo} />
+        <div>
+          <center>
+            <img src={logoTitle} />
+          </center>
         </div>
-      </Router>
+
+
     );
   }
 }
