de406a9fbd3d65775d0dd8ef11ad1bf7d0fa4eb6
hncleary <clearyhunter@gmail.com>
2019-10-29 14:01:53 -0500

    MERN Shit Boiiiii

---
+++ MERN-Version/src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          This app is gonna be no good.
+        </p>
+        <p>
+        Testing testing 123.
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
