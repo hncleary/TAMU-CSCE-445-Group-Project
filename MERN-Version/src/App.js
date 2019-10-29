3517eacc472c0e8496b0aa671f74d5d0023ecc45
hncleary <clearyhunter@gmail.com>
2019-10-29 15:39:34 -0500

    Pure HTML Version

--- MERN-Version/src/App.js
+++ MERN-Version/src/App.js
@@ -1,11 +1,8 @@
 import React, { Component } from "react";
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+// import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 
-import CreateTodo from "./components/create-todo.component";
-import EditTodo from "./components/edit-todo.component";
-import TodosList from "./components/todos-list.component";
 import { slide as Menu } from 'react-burger-menu';
 
 import logo from "./images/fmf-logo.png";
